7fae3f02481b4588049d477b69c7673ea903a134
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-02-06 10:51:56 -0300

    Create d.cs

---
+++ lista09/d.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+namespace aaa
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[] v = {1, 5, 10};
+			
+			Console.WriteLine(v.Max());
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
